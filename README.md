9f0265956a51bfc0f3e5ffd1abece5ef889ff396
Omar Fadl <omar_fadl@outlook.com>
2021-12-11 23:31:43 +0200

    Edited the README file and the source code docstrings

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # Master-MicroTask01_function_plotter
+
+Function Plotter is a GUI designed with PyQT5 for Task01 for the Junior Software Engineer Position at Master-Micro. It is designed to take a mathematical function input from the user with its boundaries, then draws this function.
+
+## Installation
+
+Clone the package in the needed directory by using this command
+
+```bash
+git clone https://github.com/OmarMostafaFadl/Master-MicroTask01_function_plotter.git
+```
+
+## Source Codes
+The script Task01_Function_Plotter.py contains all the source code
+
+## Usage
+
+```python
+# Solves the input function
+solve_function()
+
+# Plots the function
+plot_function()
+
+# Adds the start button to the GUI
+start_button()
+
+# Gets called when the start button is clicked to get the users input
+start_button_clicked
+```
+
+Please open the Python scripts and read the functions' docstrings for further understanding.
--- Task01_Function_Plotter.py
+++ Task01_Function_Plotter.py
@@ -6,12 +6,29 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QApplication, QMainWindow, QInputDialog
+from PyQt5.QtWidgets import QApplication, QMainWindow, QInputDialog, QWidget, QPushButton, QMessageBox
+from PyQt5.QtCore import pyqtSlot
 
 class UsersFunction():
+    
+    """This class contains all the functions used to solve and plot the user entered funciton
+    """
 
     def solve_function(text, u_min, u_max):
 
+        """
+        This function takse a Mathematical function, Minimum boundary and Maximum Boundary and solves them
+        to return all the X and Y points of the function within the boundary
+
+        Inputs : 
+            String: text
+            Int: u_min
+            Int: u_max
+
+        Returns:
+            List: x_iters
+            List: y_iters
+        """
         x = var('x')  # the possible variable names must be known beforehand...
         user_input = text
         expr = sympify(user_input)
@@ -29,6 +46,17 @@ class UsersFunction():
 
     def plot_function(x_iters, y_results):
 
+        """
+        This function takes all the X and Y points and plots the graph
+        
+        Inputs : 
+            List: x_iters
+            List: y_iters
+
+        Returns:
+            None
+        """
+
         plt.plot(x_iters, y_results, 'b')
         plt.show()
 
@@ -36,25 +64,75 @@ class UsersFunction():
 
 class MyWindow(QMainWindow, UsersFunction):                  #Inherites everything from QMainWindow
 
+    """This class is for the GUI
+    """
+
     def __init__(self):
+
+        """This Function is called whenever an object of MyWindow is made.
+        """
+
         super(MyWindow, self).__init__()
-        self.setGeometry(200, 100, 1500, 900)
+        self.setGeometry(200, 100, 400, 400)
         self.setWindowTitle('Function Plotter')
         self.initUI()
 
     def initUI(self):
+
+        """This Function initilizes the UI
+        """
+
+        self.start_button()
         
-        text = self.get_function()
-        u_min, u_max = self.get_boundaries()
-        x_iters, y_results = UsersFunction.solve_function(text, u_min, u_max)
-        UsersFunction.plot_function(x_iters, y_results)
+
+    def start_button(self):
+
+        """This function add a Start button to the GUI and calls another function whenever
+        this button is clicked
+        """
+
+        button = QPushButton('Press me to Start', self)
+        button.setToolTip('Press this button to add a function to plot')
+        button.move(100,70)
+        button.resize(200, 200)
+        button.clicked.connect(self.start_button_clicked)
+
+    def start_button_clicked(self):
+
+        """When the button is clicked this function is called to get all inputs from the user
+        """
+
+        try:
+
+            text = self.get_function()
+            u_min, u_max = self.get_boundaries()
+            x_iters, y_results = UsersFunction.solve_function(text, u_min, u_max)
+            UsersFunction.plot_function(x_iters, y_results)
+
+        except:
+            QMessageBox.question(self, "Error Message", "Please Enter the Data Correctly", QMessageBox.Ok, QMessageBox.Ok)
 
     def get_function(self):
+
+        """This function gets a string from the user which is the math function.
+
+        Returns:
+            String: users_function
+        """
+
         users_function, result = QInputDialog.getText(self, 'input Function', 'Please Enter Your Function')
 
         return users_function
 
     def get_boundaries(self):
+
+        """This function takes from the user the function boundaries
+
+        Returns:
+            Int: users_min
+            Int_ users_max
+        """
+
         users_min, result = QInputDialog.getText(self, 'Input Min and Max', 'Please Enter Your Min')
         users_max, result = QInputDialog.getText(self, 'Input Min and Max', 'Please Enter Your Max')
 
